14289bb49f69ee912a0b4fee375fdab462f752c4
deijahmonay <deijah@shack15.com>
2024-12-05 06:08:22 -0800

    create delete and update perfume routes, + export

--- controllers/perfumes.js
+++ controllers/perfumes.js
@@ -29,7 +29,6 @@ router.get('/:perfumeId', async (req, res) => {
     const foundPerfume = await Perfume.findById(req.params.perfumeId)
     if(!foundPerfume) {
     return res.status(404).json({ error: 'Perfume not found'})
-      throw new Error('Perfume not found')
     }
     res.status(200).json(foundPerfume)
   }catch(err) {
@@ -41,8 +40,40 @@ router.get('/:perfumeId', async (req, res) => {
   }
 })
 
+router.delete('/:perfumeId', async (req, res) => {
+  try{
+    const deletedPerfume = await Perfume.findByIdAndDelete(req.params.perfumeId)
+    if(!deletedPerfume) {
+      res.status(404)
+      throw new Error('Perfume not found')
+    }
+    res.status(200).json(deletedPerfume)
+  }catch(err) {
+    if (res.statusCode === 404) {
+      res.json({ error: err.message })
+    }else {
+      res.status(500).json({error: err.message})
+    }
+  }
+}
+)
 
+router.put('/:perfumeId', async (req, res) => {
+  try{
+    const updatedOerfume = await Perfume.findByidAndUpdate(req.params.perfumeId, req.body, {new: true})
+    if(!updatedPerfume) {
+      throw new Error('Perfume not found')
+    } res.status(200).json(updatedPerfume)
+  }catch(err) {
+    if (res.statusCode === 400) {
+      res.json({ error: err.message })
+    }else {
+      res.status(500).json({error: err.message})
+    }
+  }
+})
 
+module.exports = router;
 
 
 
