02f36ecfe345476b6dc16aaa3b10bb90cb78c6cb
deijahmonay <deijah@shack15.com>
2024-12-05 17:11:56 -0800

    import bcrypt and user model

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 // controllers/users.js
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 // router.post('/signup', (req, res))
 router.post('/signup', (req, res) => {
