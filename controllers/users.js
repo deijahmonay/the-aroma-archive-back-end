224d3d6e7dc9eb46c97d2c8b2280dca0fc29b95f
deijahmonay <deijah@shack15.com>
2024-12-05 19:30:28 -0800

    complete sognin route

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const SALT_LENGTH = 12
 
@@ -28,11 +29,22 @@ router.post('/signup', async (req, res) => {
   }
 })
 
-router.post('/sign', async (req, res) => {
+router.post('/signin', async (req, res) => {
   try{
-
+    const user = await User.findOne({ 
+      $or: [
+        { username: req.body.username },
+        { email: req.body.email }
+      ]
+     })
+    if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+      const token = jwt.sign({username: user.username, _id: user._id}, process.env.JWT_SECRET)
+      res.status(200).json({ token })
+    } else {
+      res.status(401).json({ message: 'Invalid username, email or password!' })
+    }
   }catch(err) {
-    
+    res.status(400).json({ error: err.message })
   }
 })
 
