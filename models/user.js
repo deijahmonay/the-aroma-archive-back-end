0981fed9d3542305f3fb7c28724c01637bd33579
deijahmonay <deijah@shack15.com>
2024-12-05 18:27:49 -0800

    remove hashedPassword from response

--- models/user.js
+++ models/user.js
@@ -19,6 +19,9 @@ const userSchema =  mongoose.Schema({
   }
 })
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = User;
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.hashedPassword
+  }
+})
+module.exports = mongoose.model('User', userSchema)
