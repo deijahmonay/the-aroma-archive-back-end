c38549a3f651d038641572f81927a5f0da6c297b
deijahmonay <deijah@shack15.com>
2024-12-05 20:30:52 -0800

    complete verify token auth

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const verifyToken = require('../middleware/verify-token')
 
-router.get('/:userId', async (req, res) => {
+router.get('/:userId', verifyToken, async (req, res) => {
   try {
     const user = await User.findById(req.params.userId);
     if (!user) {
--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken")
 
-function verifyToken(req, res, nest) {
+function verifyToken(req, res, next) {
   try {
     const token = req.headers.authorization.split(' ')[1]
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
