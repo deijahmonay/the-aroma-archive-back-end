db52e403451c0c1f21699c445744caa2b9c9b779
deijahmonay <deijah@shack15.com>
2024-12-05 17:48:03 -0800

    add user sign up function to users.js controller

--- controllers/users.js
+++ controllers/users.js
@@ -4,9 +4,28 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
-// router.post('/signup', (req, res))
-router.post('/signup', (req, res) => {
-  res.json({ message: 'Signup route' })
+const SALT_LENGTH = 12
+
+router.post('/signup', async (req, res) => {
+  try {
+    const userInDatabase = await User.findOne({ username: req.body.username })
+    if (userInDatabase) {
+      return res.status(400).json({error: 'Username already taken. Please try another username.'})
+    }
+
+    const emailInDatabase = await User.findOne({ email: req.body.email })
+    if (emailInDatabase) {
+      return res.status(400).json({ error: 'Email already taken. Please try another email.'})
+    }
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
+    })
+    res.status(201).json({ user })
+  }catch(err) {
+    res.status(400).json({error: err.message})
+  }
 })
 
 module.exports = router
